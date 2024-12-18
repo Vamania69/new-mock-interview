8897a1920348af60dcf502040851983c3cfbd0aa
varun <varunagrawal6261@gmail.com>
2024-12-18 14:08:12 +0530

    build issue

--- src/app/api/users/upload-resume/route.ts
+++ src/app/api/users/upload-resume/route.ts
@@ -2,7 +2,6 @@ import { data } from '@/constants/data';
 import { PrismaClient } from '@prisma/client';
 import multer from 'multer';
 import { NextResponse } from 'next/server';
-import { PDFDocument } from 'pdf-lib';
 const prisma = new PrismaClient();
 
 // Set up Multer to handle file uploads in memory
@@ -17,42 +16,42 @@ export const config = {
 export async function POST(req: Request) {
     try {
         // Use Multer to handle the file upload
-        await new Promise((resolve, reject) => {
-            upload.single('file')(req, {}, (err) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(undefined);
-                }
-            });
-        });
+        // await new Promise((resolve, reject) => {
+        //     upload.single('file')(req, {}, (err) => {
+        //         if (err) {
+        //             reject(err);
+        //         } else {
+        //             resolve(undefined);
+        //         }
+        //     });
+        // });
 
 
-        console.log('file-------------')
-        console.log(req.body, 'file-------------')
-        const file = req.file;
-        // if (!file) {
-        //     return NextResponse.json({ error: 'No file uploaded.' }, { status: 400 });
-        // }
+        // console.log('file-------------')
+        // console.log(req.body, 'file-------------')
+        // const file = req.file;
+        // // if (!file) {
+        // //     return NextResponse.json({ error: 'No file uploaded.' }, { status: 400 });
+        // // }
 
-        // Extract text using pdf-lib
-        const pdfBytes = await file.buffer;
-        const pdfDocument = await PDFDocument.load(pdfBytes);
-        console.log(pdfBytes, 'data---------------logs')
-        const extractedText = await pdfDocument.getTextContent();
+        // // Extract text using pdf-lib
+        // const pdfBytes = await file.buffer;
+        // const pdfDocument = await PDFDocument.load(pdfBytes);
+        // console.log(pdfBytes, 'data---------------logs')
+        // const extractedText = await pdfDocument.getTextContent();
 
-        // const userId = parseInt(req.body.userId);
-        // if (isNaN(userId)) {
-        //     return NextResponse.json({ error: 'Invalid user ID.' }, { status: 400 });
-        // }
+        // // const userId = parseInt(req.body.userId);
+        // // if (isNaN(userId)) {
+        // //     return NextResponse.json({ error: 'Invalid user ID.' }, { status: 400 });
+        // // }
 
-        await prisma.user.update({
-            where: { id: userId },
-            data: {
-                resumeUploaded: true,
-                resumeData: extractedText,
-            },
-        });
+        // await prisma.user.update({
+        //     where: { id: userId },
+        //     data: {
+        //         resumeUploaded: true,
+        //         resumeData: extractedText,
+        //     },
+        // });
 
         return NextResponse.json({ message: 'File uploaded and data extracted successfully.', data: data }, { status: 200 });
     } catch (error) {
